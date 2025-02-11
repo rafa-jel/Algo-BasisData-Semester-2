3c48703e95f0bf0ab6782443be0929f4042703f8
rafa-jel <rafaradana2@gmail.com>
2025-02-11 10:24:22 +0700

    Update Perulangan.java

--- Week 1/Perulangan.java
+++ Week 1/Perulangan.java
@@ -2,9 +2,26 @@ import java.util.Scanner;
 public class Perulangan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int NIM;
+        Long NIM;
 
         System.out.println("==============================");
-        System.out.print("Masukan NIM : "); NIM = sc.nextInt();
+        System.out.print("Masukan NIM : "); NIM = sc.nextLong();
+
+        NIM = NIM % 100;
+
+        if (NIM < 10) {
+            NIM += 10;
+        }
+
+        for (int i = 1; i <= NIM; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            }
+            if (i % 2 == 1) {
+                System.out.print("* ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
