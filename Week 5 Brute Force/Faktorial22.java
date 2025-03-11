d3415188f0cbe5a893ed5c71517c767c9d40ea1f
rafa-jel <rafaradana2@gmail.com>
2025-03-11 11:12:46 +0700

    Week 5

--- Week 5 Brute Force/Faktorial22.java
+++ Week 5 Brute Force/Faktorial22.java
@@ -2,8 +2,10 @@ public class Faktorial22 {
 
     int faktorialBF(int n) {
         int fakto = 1;
-        for(int i=1; i<=n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
