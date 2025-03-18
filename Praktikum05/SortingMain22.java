1cd79de54b252682710025dd1482444075cf7765
rafa-jel <rafaradana2@gmail.com>
2025-03-18 12:54:04 +0700

    Week 6
    Sorting

--- Praktikum05/SortingMain22.java
+++ Praktikum05/SortingMain22.java
@@ -2,6 +2,8 @@ public class SortingMain22 {
     public static void main(String[] args) {
         
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
 
         Sorting22 dataurut1 = new Sorting22(a, a.length);
 
@@ -10,5 +12,21 @@ public class SortingMain22 {
         dataurut1.bubbleSort();
         System.out.println("DATA SUDAH DIURUTKAN DENGAN BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        Sorting22 dataurut2 = new Sorting22(b, b.length);
+
+        System.out.println("Data Awal 2");
+        dataurut2.tampil();
+        dataurut2.bubbleSort();
+        System.out.println("DATA SUDAH DIURUTKAN DENGAN SELECTION SORT (ASC)");
+        dataurut2.tampil();
+
+        Sorting22 dataurut3 = new Sorting22(c, c.length);
+
+        System.out.println("Data Awal 3");
+        dataurut3.tampil();
+        dataurut3.bubbleSort();
+        System.out.println("DATA SUDAH DIURUTKAN DENGAN INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
