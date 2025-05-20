a38efc8ff146450e90b3de31cea2ec91de4be287
rafa-jel <rafaradana2@gmail.com>
2025-05-20 09:36:56 +0700

    Week 13

--- Pertemuan 13/DoubleLinkedList.java
+++ Pertemuan 13/DoubleLinkedList.java
@@ -1,3 +1,5 @@
+import java.io.EOFException;
+
 public class DoubleLinkedList {
     Node head;
     int size;
@@ -85,5 +87,57 @@ public class DoubleLinkedList {
         }
     }
 
-    
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat menghapus");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat menghapus");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
 }
